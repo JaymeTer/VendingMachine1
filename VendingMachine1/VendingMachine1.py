8c07fd49d8fe22c5fad6a21de8d2e65469f88818
chris <christophervarner30@icloud.com>
2025-01-17 14:07:05 -0800

    I had to fix added comments and function to add more items later

--- VendingMachine1/VendingMachine1.py
+++ VendingMachine1/VendingMachine1.py
@@ -1,35 +1,31 @@
-
-def display_menu():
+def display_menu(items):  # More flexible with a list of items
     """Displays the vending machine menu."""
     print("Welcome to the Vending Machine!")
-    print("Please select an item by entering the corresponding number:")
-    print("1. Cola")
-    print("2. Chips")
-    print("3. Candy")
-    print("4. Water")
+    for i, item in enumerate(items):
+        print(f"{i+1}. {item}")
 
-def get_user_selection():
+def get_user_selection(num_items):  # Validates based on number of items
     """Prompts the user to make a selection and returns the selection."""
     while True:
         try:
             selection = int(input("Enter your selection: "))
-            if selection < 1 or selection > 4:
-                print("Invalid selection. Please enter a number between 1 and 4.")
-            else:
+            if 1 <= selection <= num_items:
                 return selection
+            else:
+                print(f"Invalid selection. Please enter a number between 1 and {num_items}.")
         except ValueError:
             print("Invalid input. Please enter a number.")
 
-def vend_item(selection):
+def vend_item(selection, items):  # Uses the item list
     """Simulates vending the selected item."""
-    items = ["Cola", "Chips", "Candy", "Water"]
-    print(f"Vending {items[selection - 1]}... Enjoy your {items[selection - 1]}!")
+    print(f"Vending {items[selection - 1]}... Enjoy!")
 
 def main():
     """Main function to run the vending machine simulation."""
-    display_menu()
-    selection = get_user_selection()
-    vend_item(selection)
+    items = ["Cola", "Chips", "Candy", "Water"]  # Now you can easily add more!
+    display_menu(items)
+    selection = get_user_selection(len(items))
+    vend_item(selection, items)
 
 if __name__ == "__main__":
     main()
